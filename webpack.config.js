701ebee0b15c2b22a6600ebd38a965e895adb56b
Yangsibai <yangsibai@gmail.com>
2016-03-16 22:14:33 +0800

    add missing package

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react']
                 }
             },
             // LESS
